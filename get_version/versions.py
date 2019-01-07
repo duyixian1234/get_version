73e7b4bedc25ea12bd323fb2bd6f97ad6bb0e5a3
duyixian <duyixian@meideng.net>
2019-01-07 10:27:00 +0800

    support more

--- get_version/versions.py
+++ get_version/versions.py
@@ -1,1 +1,7 @@
-version_dict = {'python': '-V', 'python3': '-V', 'node': '', 'npm': '', 'yarn': '', 'java': '', 'gcc': ''}
+python_versions = {'python': '-V', 'python3': '-V', 'pip': '-V'}
+node_versions = {'node': '-v', 'npm': '-v', 'yarn': '-v'}
+java_versions = {'java': '-version', 'mvn': '-v', 'gradle': '-v'}
+cxx_versions = {'gcc': '-v', 'clang': '-v'}
+go_versions = {'go': 'version'}
+
+version_dict = {**python_versions, **node_versions, **java_versions, **cxx_versions, **go_versions}
